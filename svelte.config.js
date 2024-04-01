f483751cb714621c12d6261dc727758ac5050da9
Richard Atterfalk <richard@atterfalk.se>
2024-04-01 14:31:19 +0200

    Updated svelte config with a few more path-variables.

--- svelte.config.js
+++ svelte.config.js
@@ -20,10 +20,13 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/blog'
+			base: process.argv.includes('dev') ? '' : '/blog',
+			assets: process.argv.includes('dev') ? '' : '/blog'
 		}
 	}
 };
