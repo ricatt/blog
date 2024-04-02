b4ac2053aeae1a627f07773075cb785cad5687f0
Richard Atterfalk <richard@atterfalk.se>
2024-04-02 21:09:59 +0200

    Updated page with a slightly less boilerplate-message.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Testing github pages</h1>
+<p>Wanted to see if I could pages working with Svelte, since I've never done that before.</p>
